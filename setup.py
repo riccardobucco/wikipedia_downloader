532d2c4ae9ee74fbe74115d08366e1cb6528d017
Riccardo Bucco <riccardobucco@gmail.com>
2018-12-30 18:42:35 +0100

    Conform to naming style

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@
 import setuptools
 
 with open('README.md') as f:
-    long_description = f.read()
+    LONG_DESCRIPTION = f.read()
 
 setuptools.setup(
     name="wikipedia_downloader",
     version="0.1",
     author="Riccardo Bucco",
     description="Download Wikipedia data dumps",
-    long_description=long_description,
+    long_description=LONG_DESCRIPTION,
     long_description_content_type="text/markdown",
     url="https://github.com/riccardobucco/wikipedia_downloader",
     author_email="riccardobucco@gmail.com",
